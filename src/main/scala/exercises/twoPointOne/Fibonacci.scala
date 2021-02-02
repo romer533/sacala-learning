b9ed93e0ee03e1712673681123fe18f2c870d83c
RomanGoltsov <romer533@mail.ru>
2021-02-02 22:53:12 +0600

    Fibonacci update
    Fibonacci V2.0

--- src/main/scala/exercises/twoPointOne/Fibonacci.scala
+++ src/main/scala/exercises/twoPointOne/Fibonacci.scala
@@ -9,4 +9,10 @@ object Fibonacci extends App {
     else fib(n - 1) + fib(n - 2)
   }
 
+  // Versional 2.0
+  val fib2: Int => Int =
+    n => if (n <= 1) n else fib2(n - 1) + fib2(n - 2)
+
+  println(fib2(5))
+
 }
