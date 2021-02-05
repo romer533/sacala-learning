8f0228360446344afd1c6ade2981116dc3dd9717
RomanGoltsov <romer533@mail.ru>
2021-02-05 14:03:51 +0600

    Update DropWhile.scala
    * Change DropWhile

--- src/main/scala/exercises/chapter3/fifth/DropWhile.scala
+++ src/main/scala/exercises/chapter3/fifth/DropWhile.scala
@@ -1,10 +1,13 @@
 package exercises.chapter3.fifth
 
+import exercises.chapter3.{Cons, MyList}
+
 object DropWhile extends App {
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
-    case _ :: xs => {
-      ???
+  def dropWhile[A](l: MyList[A], f: A => Boolean): MyList[A] = l match {
+    case Cons(h, t) => {
+      if (f(h)) t
+      else Cons(h, t)
     }
   }
 
