a2f1102860f18c9ca8b98c3559087beee5e1f4d1
RomanGoltsov <romer533@mail.ru>
2021-02-05 13:57:49 +0600

    Update Drop.scala
    * Change Drop

--- src/main/scala/exercises/chapter3/fourth/Drop.scala
+++ src/main/scala/exercises/chapter3/fourth/Drop.scala
@@ -1,14 +1,16 @@
 package exercises.chapter3.fourth
 
+import exercises.chapter3.{Cons, MyList}
+
 import scala.annotation.tailrec
 
 object Drop extends App {
 
   @tailrec
-  def drop[A](l: List[A], n: Int): List[A] = l match {
-    case _ :: xs => {
-      if (n <= 0) xs
-      else drop(xs, n - 1)
+  def drop[A](l: MyList[A], n: Int): MyList[A] = l match {
+    case Cons(_, t) => {
+      if (n <= 0) t
+      else drop(t, n - 1)
     }
   }
 
