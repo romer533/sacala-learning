21e9ff32924996c9bef2696cb1b26624b7568480
RomanGoltsov <romer533@mail.ru>
2021-02-05 16:56:37 +0600

    Update DropWhile.scala

--- src/main/scala/exercises/chapter3/fifth/DropWhile.scala
+++ src/main/scala/exercises/chapter3/fifth/DropWhile.scala
@@ -2,13 +2,17 @@ package exercises.chapter3.fifth
 
 import exercises.chapter3.{Cons, MyList}
 
+import scala.annotation.tailrec
+
 object DropWhile extends App {
 
+  @tailrec
   def dropWhile[A](l: MyList[A], f: A => Boolean): MyList[A] = l match {
-    case Cons(h, t) => {
-      if (f(h)) t
-      else Cons(h, t)
-    }
+    case Cons(h, t) =>
+      if (f(h)) l
+      else dropWhile(t, f)
   }
+
+  println(dropWhile(MyList(1, 2, 3, 4), (f: Int) => f > 2))
 
 }
