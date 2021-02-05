60130f8d3f7b6950ea48969bfe73b23165949145
RomanGoltsov <romer533@mail.ru>
2021-02-05 13:54:14 +0600

    Update SetHead.scala
    * Change setHead

--- src/main/scala/exercises/chapter3/third/SetHead.scala
+++ src/main/scala/exercises/chapter3/third/SetHead.scala
@@ -1,10 +1,12 @@
 package exercises.chapter3.third
 
+import exercises.chapter3.{Cons, MyList}
+
 object SetHead extends App {
 
-  def setHead[A](as: List[A]) = as match {
-    case x :: _ => x
-    case Nil => ???
+  def setHead[A](as: MyList[A]) = as match {
+    case Cons(h, _) => h
+//    case Nil => ???
   }
 
 }
