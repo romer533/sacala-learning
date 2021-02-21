38cd68993531d5acd7c22169d8c2382f3b06110f
RomanGoltsov <romer533@mail.ru>
2021-02-21 14:37:33 +0600

    Create ChatRoom.scala

--- src/main/scala/akka/chatRoom/ChatRoom.scala
+++ src/main/scala/akka/chatRoom/ChatRoom.scala
@@ -1,7 +1,43 @@
 package akka.chatRoom
 
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
 object ChatRoom {
 
   private final case class PublishSessionMessage(screenName: String, message: String) extends RoomCommand
+
+  def apply(): Behavior[RoomCommand] = chatRoom(List.empty)
+
+  private def chatRoom(sessions: List[ActorRef[SessionCommand]]): Behavior[RoomCommand] =
+    Behaviors.receive { (context, message) =>
+      message match {
+        case GetSession(screenName, client) =>
+          val ses = context.spawn(
+            session(context.self, screenName, client),
+            name = URLEncoder.encode(screenName, StandardCharsets.UTF_8.name))
+          client ! SessionGranted(ses)
+          chatRoom(ses :: sessions)
+        case PublishSessionMessage(screenName, message) =>
+          val notification = NotifyClient(MessagePosted(screenName, message))
+          sessions.foreach(_ ! notification)
+          Behaviors.same
+      }
+    }
+
+  private def session(room: ActorRef[PublishSessionMessage],
+                      screenName: String,
+                      client: ActorRef[SessionEvent]): Behavior[SessionCommand] =
+    Behaviors.receiveMessage {
+      case PostMessage(message) =>
+        room ! PublishSessionMessage(screenName, message)
+        Behaviors.same
+      case NotifyClient(message) =>
+        client ! message
+        Behaviors.same
+    }
 
 }
