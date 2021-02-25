4576c7fcb52b240b8882aa1e3ba4945952e4aeb2
RomanGoltsov <romer533@mail.ru>
2021-02-25 16:16:19 +0600

    Update ChatRoom.scala

--- src/main/scala/akka/chatRoom/ChatRoom.scala
+++ src/main/scala/akka/chatRoom/ChatRoom.scala
@@ -1,6 +1,6 @@
 package akka.chatRoom
 
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, Behavior}
 
 import java.net.URLEncoder
@@ -10,7 +10,8 @@ object ChatRoom {
 
   private final case class PublishSessionMessage(screenName: String, message: String) extends RoomCommand
 
-  def apply(): Behavior[RoomCommand] = chatRoom(List.empty)
+  def apply(): Behavior[RoomCommand] =
+    Behaviors.setup(context => new ChatRoomBehavior(context))
 
   private def chatRoom(sessions: List[ActorRef[SessionCommand]]): Behavior[RoomCommand] =
     Behaviors.receive { (context, message) =>
@@ -28,6 +29,27 @@ object ChatRoom {
       }
     }
 
+  def aga[A](as: List[Option[A]]): Option[List[A]] = {
+    val empty: Option[List[A]] = Some(Nil)
+    as.foldLeft(empty) { (acc, el) =>
+      el match {
+        case Some(value) =>
+          acc match {
+            case Some(list) =>
+              Some(value +: list)
+            case None => None
+          }
+        case None => None
+      }
+
+    }
+  }
+
+  def aga2[A](as: List[Option[A]]): Option[List[A]] = {
+    val empty: Option[List[A]] = Some(Nil)
+    as.foldLeft(empty)((acc, el) => el.flatMap(value => acc.map(list => list :+ value)))
+  }
+
   private def session(room: ActorRef[PublishSessionMessage],
                       screenName: String,
                       client: ActorRef[SessionEvent]): Behavior[SessionCommand] =
@@ -39,5 +61,11 @@ object ChatRoom {
         client ! message
         Behaviors.same
     }
+
+  class ChatRoomBehavior(context: ActorContext[RoomCommand]) extends AbstractBehavior[RoomCommand](context) {
+    private var session: List[ActorRef[SessionCommand]] = List.empty
+
+    override def onMessage(msg: RoomCommand): Behavior[RoomCommand] = ???
+  }
 
 }
